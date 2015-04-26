07a1defabae0484f1f567945b4f0f5d38c79ae54
snorrik <snorrik@rambler.ru>
2015-04-26 16:49:55 +0400

    My commit for coursera 2

--- cachematrix.R
+++ cachematrix.R
@@ -22,7 +22,7 @@ cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
     inv <- x$getinverse()
     if(!is.null(inv)) {
-        message("Getting cached data.")
+        message("Getting cached data!")
         return(inv)
     }
     data <- x$get()
